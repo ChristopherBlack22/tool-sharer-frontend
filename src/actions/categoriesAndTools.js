1249b01fa15f011147b8a8048375112103d3257d
Christopher <christopher.black.2209@googlemail.com>
2022-02-22 02:47:08 +0000

    fetching status moved from StatusReducer to ToolReducer

--- src/actions/categoriesAndTools.js
+++ src/actions/categoriesAndTools.js
@@ -5,7 +5,7 @@ export const fetchCategoriesAndTools = () => {
     let token = localStorage.getItem("jwt");
     return (dispatch) => {
         dispatch({type: "FETCHING_CATEGORIES"}); //sending an action to the reducer to indicate request made
-        dispatch({type: "CATEGORIES_AND_TOOLS_GET_REQUEST"});
+        dispatch({type: "FETCHING_TOOLS"});
 
         fetch("http://localhost:3001/api/v1/categories", {
             method: "GET",
