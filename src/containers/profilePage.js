cd9aa9568a9b788fbc0e9359e6c6784f10c81e7b
Christopher <christopher.black.2209@googlemail.com>
2022-02-28 03:10:44 +0000

    Layout of Profile page created

--- src/containers/profilePage.js
+++ src/containers/profilePage.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import ToolsContainer from './toolsContainer';
+import Tool from '../components/tool';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class ProfilePage extends Component {
   render() {
+    // const url = this.props.match.url;
     return (
       <div className="profile-page">
-        <p>Profile Page rendering</p>
+        <h1>Hello {this.props.user.username}!</h1>
+        <div>These are the Tools you are sharing (allow delete)</div>
+        {/* <ToolsContainer ownerId={this.props.user.id} tools={this.props.tools} parentUrl={url} /> */}
+        <div>These are the Tools you are borrowing (allow return)</div>       
+        {/* <ToolsContainer borrowerId={this.props.user.id} tools={this.props.tools} parentUrl={url} /> */}
+        
+        <div>Form to add a new tool to share</div>
+        
+        {/* <Route exact path={url} render={ () => <h3><em>Select Tool to View</em></h3>} />
+        <Route path={`${url}/:tool_id`} render={ (routerProps) => <Tool tools={this.props.tools} {...routerProps} />} /> */}
       </div>
     )
   }
 
 }
 
-export default ProfilePage;
+const mapStateToProps = state => {
+  return {
+    user: state.currentUser,
+    tools: state.tools.toolsArray
+  }
+}
+
+export default connect(mapStateToProps)(ProfilePage);
