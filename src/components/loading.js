a4d651054c238d7804587c794eae234b9889edb8
Christopher <christopher.black.2209@googlemail.com>
2022-03-03 23:02:53 +0000

    Loading component added for potential reuse

---
+++ src/components/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return(
+        <div className="loading" >
+            <em>...LOADING...</em>
+        </div>
+    )
+}
+
+export default Loading;
--- src/containers/categoriesContainer.js
+++ src/containers/categoriesContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loading from '../components/loading';
 import Category from '../components/category';
 
 const CategoriesContainer = ({fetchingCategories, categories}) => {
@@ -6,9 +7,7 @@ const CategoriesContainer = ({fetchingCategories, categories}) => {
     //stop rendering when categories is initally an empty array or before the fetch is completed. 
     if(fetchingCategories || categories.length === 0) {
         return(
-            <div className="loading" >
-                <em>...LOADING...</em>
-            </div>
+            <Loading/>
         )
     } else {
         let categoriesCards;
