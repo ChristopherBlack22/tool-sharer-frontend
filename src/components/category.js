663715f426918fac3fdd33b2d8be09837d70c181
Christopher <christopher.black.2209@googlemail.com>
2022-03-03 23:35:48 +0000

    category refactored as function component

--- src/components/category.js
+++ src/components/category.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-// class Category extends Component {
 const Category = ({category}) => {
 
     const {name, image_url} = category;
