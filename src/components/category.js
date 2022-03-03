660353dc8bba958eea834349b04212d65999a3cf
Christopher <christopher.black.2209@googlemail.com>
2022-03-03 23:35:08 +0000

    category refactored as function component

--- src/components/category.js
+++ src/components/category.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class Category extends Component {
+// class Category extends Component {
+const Category = ({category}) => {
 
-    render() {
-        const {name, image_url} = this.props.category;
-        return (
-            <div className="category-card">
-                <Link to={`/${name}/tools`} >
-                    <h3>{name}</h3>
-                    <img src={image_url} width="150" height="150" alt={name} />
-                </Link>
-            </div>
-        )
-    }
+    const {name, image_url} = category;
+    return (
+        <div className="category-card">
+            <Link to={`/${name}/tools`} >
+                <h3>{name}</h3>
+                <img src={image_url} width="150" height="150" alt={name} />
+            </Link>
+        </div>
+    )
 
 }
 
