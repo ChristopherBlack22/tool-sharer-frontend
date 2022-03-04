690ca22519b0fe2698315113172da0d7562f72b5
Christopher <christopher.black.2209@googlemail.com>
2022-03-04 00:10:46 +0000

    Tool refactored into function component

--- src/components/tool.js
+++ src/components/tool.js
@@ -1,32 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UpdateButton from './updateButton';
 import DeleteButton from './deleteButton';
 
-class Tool extends Component {
+const Tool = ({match, tools, currentUserId}) => {
 
-    findTool = () => {
-        let tool;
-        tool = this.props.tools.find(tool => tool.id === parseInt(this.props.match.params.tool_id));
-        return tool;
-    }
-
-    render() {
-        
-        const tool = this.findTool();
-        
-        return (
-            <div className="tool" >
-                <h2>{tool.name}</h2>
-                <p>{tool.description}</p>
-                <img src={tool.image_url} alt={tool.name} width="280" />
-                {tool.owner.id === this.props.currentUserId ? <p>Owner: You</p> : <p>Owner: {tool.owner.username}</p>}
-                {tool.borrower ? <p>Currently Unavailable</p> : <p>Currently Available</p> }
-                {(tool.borrower && tool.borrower.id === this.props.currentUserId) ? <p><small>This Tool is with You</small></p> : null }
-                <UpdateButton tool={tool} currentUserId={this.props.currentUserId} />
-                <DeleteButton tool={tool} currentUserId={this.props.currentUserId} />
-            </div>
-        )
-    }
+    const tool = tools.find(tool => tool.id === parseInt(match.params.tool_id));
+    return (
+        <div className="tool" >
+            <h2>{tool.name}</h2>
+            <p>{tool.description}</p>
+            <img src={tool.image_url} alt={tool.name} width="280" />
+            {tool.owner.id === currentUserId ? <p>Owner: You</p> : <p>Owner: {tool.owner.username}</p>}
+            {tool.borrower ? <p>Currently Unavailable</p> : <p>Currently Available</p> }
+            {(tool.borrower && tool.borrower.id === currentUserId) ? <p><small>This Tool is with You</small></p> : null }
+            <UpdateButton tool={tool} currentUserId={currentUserId} />
+            <DeleteButton tool={tool} currentUserId={currentUserId} />
+        </div>
+    )
 
 }
 
