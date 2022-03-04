324fbbb2a482a29e0d72524bd7caf5017eb9a79f
Christopher <christopher.black.2209@googlemail.com>
2022-03-04 00:16:02 +0000

    ToolsList refactored into function component

--- src/components/toolsList.js
+++ src/components/toolsList.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class ToolsList extends Component {
-
-    render() {
-        const toolList = this.props.tools.map(tool => {
-            let availability;
-            if(tool.borrower) {
-                availability = <span className="available">Unavailable</span>;
-            } else {
-                availability = <span className="unavailable">Available</span>;
-            }
-            return(
-                <li key={tool.id} >
-                    <Link key={tool.id} to={`${this.props.parentUrl}/${tool.id}`}>
-                        {tool.name} - {availability}
-                    </Link>
-                </li>
-            )
-        })
-
-        return (
-            <div className="tools-list">
-                {toolList}
-            </div>
+const ToolsList = ({tools, parentUrl}) => {
+   
+    const toolList = tools.map(tool => {
+        let availability;
+        if(tool.borrower) {
+            availability = <span className="available">Unavailable</span>;
+        } else {
+            availability = <span className="unavailable">Available</span>;
+        }
+        return(
+            <li key={tool.id} className="tool-list-item">
+                <Link key={tool.id} to={`${parentUrl}/${tool.id}`}>
+                    {tool.name} - {availability}
+                </Link>
+            </li>
         )
-    }
+    })
+
+    return (
+        <div className="tools-list">
+            {toolList}
+        </div>
+    )
 
 }
 
